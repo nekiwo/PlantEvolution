d15688034b8cc4aa3d6e6ca6f9248ac7eebcff62
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-10-13 08:23:04 -0500

    work

--- src/sim/SimulatePlant.go
+++ src/sim/SimulatePlant.go
@@ -1,10 +1,21 @@
 package sim
 
 import (
+	"github.com/nekiwo/PlantEvolution/src/config"
 	"github.com/nekiwo/PlantEvolution/src/plant"
 )
 
 func SimulatePlant(data plant.Plant, ID interface{}) {
 	SimulatedPlant := data
 
+	for i := 0; i < config.TotalRays; i++ {
+		// shoot ray from the sun
+		// check for collision
+		// keep a slice of hit coords
+		// if colliding with the boundaries, terminate the ray
+		// if colliding with the segments, 90 degree bounce
+		// repeat the process, but starting from the hit point
+		// if colliding with the plant, add point
+		// send data for rendering
+	}
 }
