7de5397c30de241c14de5f01fabcf528374f5931
TemporaryNekiwo <88904411+TemporaryNekiwo@users.noreply.github.com>
2021-10-22 15:44:30 -0500

    work

--- src/sim/GetHitPoint.go
+++ src/sim/GetHitPoint.go
@@ -7,5 +7,25 @@ func GetHitPoint(ray *[2][2]int, segment *[2][2]int) [2]int {
 	// else
 	// 		return [2]int{math.Inf(1), math.Inf(1)}
 
+	// Ray
+	x1 := *ray[0][0]
+	y1 := *ray[0][1]
+	x2 := *ray[1][0]
+	y2 := *ray[1][1]
 
+	// Segment
+	x3 := *segment[0][0]
+	y3 := *segment[0][1]
+	x4 := *segment[1][0]
+	y4 := *segment[1][1]
+
+	t := (x1 - x3)(y1 - y2) - (y1 - y3)(x1 - x2) /
+		 (x1 - x2)(y3 - y4) - (y1 - y2)(x3 - x4)
+
+	u := (x1 - x3)(y1 - y2) - (y1 - y3)(x1 - x3) /
+		 (x1 - x2)(y3 - y4) - (y1 - y2)(x3 - x4)
+
+	p := [2]int{
+		
+	}
 }
