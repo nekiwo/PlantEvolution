cb684d5cbafe48933397489d45a6a2ae6af4e691
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2021-10-22 20:57:51 -0500

    work

--- src/helpers/CalcDist.go
+++ src/helpers/CalcDist.go
@@ -1,5 +1,9 @@
 package helpers
 
-func CalcDist() {
+import "math"
 
+func CalcDist(a [2]int, b [2]int) float64 {
+	return math.Sqrt(
+		float64((a[0] - b[0]) * (a[0] - b[0])) + float64((a[1] - b[1]) * (a[1] - b[1])),
+	)
 }
--- src/sim/GetHitPoint.go
+++ src/sim/GetHitPoint.go
@@ -1,31 +1,39 @@
 package sim
 
+import "math"
+
 func GetHitPoint(ray *[2][2]int, segment *[2][2]int) [2]int {
-	// if it intersects
-	// 		find coordinate
-	// 		return it
-	// else
-	// 		return [2]int{math.Inf(1), math.Inf(1)}
+	HitPoint := [2]int{math.MaxInt32, math.MaxInt32}
 
 	// Ray
-	x1 := *ray[0][0]
-	y1 := *ray[0][1]
-	x2 := *ray[1][0]
-	y2 := *ray[1][1]
+	x1 := ray[0][0]
+	y1 := ray[0][1]
+	x2 := ray[0][0] + ray[1][0]
+	y2 := ray[0][1] + ray[1][1]
 
 	// Segment
-	x3 := *segment[0][0]
-	y3 := *segment[0][1]
-	x4 := *segment[1][0]
-	y4 := *segment[1][1]
+	x3 := segment[0][0]
+	y3 := segment[0][1]
+	x4 := segment[1][0]
+	y4 := segment[1][1]
 
-	t := (x1 - x3)(y1 - y2) - (y1 - y3)(x1 - x2) /
-		 (x1 - x2)(y3 - y4) - (y1 - y2)(x3 - x4)
-
-	u := (x1 - x3)(y1 - y2) - (y1 - y3)(x1 - x3) /
-		 (x1 - x2)(y3 - y4) - (y1 - y2)(x3 - x4)
-
-	p := [2]int{
-		
+	// Denominator
+	d := (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+	if d == 0 {
+		// The line is parallel; no intersection
+		return HitPoint
 	}
+
+	t := ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / d
+	u := -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / d
+
+	if 0 < t && t < 1 && 0 < u {
+		// There is an intersection; find the hit point
+		HitPoint = [2]int{
+			x1 + t * (x2 - x1),
+			y1 + t * (y2 - y1),
+		}
+	}
+
+	return HitPoint
 }
